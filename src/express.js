2be88f301b645eee4115d89082ad118df3ccbe70
Maricarmen Vidal <maricarmenvidal204@gmail.com>
2024-02-13 09:51:34 -0500

    desafio3

--- src/express.js
+++ src/express.js
@@ -11,14 +11,27 @@ const allProducts = productManager.loadProducts()
 //console.log(await allProducts)
 
 app.get("/products",async (req,res)=>{
-    //console.log(await allProducts)
-    res.send(await allProducts)
+    const limit =parseInt(req.query.limit)
+    if(!limit){
+        return res.send(await allProducts)
+    }
+    let products= await allProducts
+    let limitProduct=products.slice(0, limit)
+    res.send(limitProduct)
 })
+
+app.get("/products/:id",async (req,res)=>{
+    const filtrarId = parseInt(req.params.id)
+    let products= await allProducts
+    let productId= products.find(product => product.id===filtrarId)
+    res.send(productId)
+})
+
 
 const port=5500
 
 const server= app.listen(port, ()=>{
-    console.log(`Express por local host ${server.address().port}`)
+    console.log(`Express en el puerto ${server.address().port}`)
 })
 server.on("error",(e)=>{
     console.log(`Error del servidor ${e}`)
